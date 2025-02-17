d2d0470b81c5c4accca799ba99fac245886a5835
Alessandro Figueiredo <66129133+alessfm@users.noreply.github.com>
2025-02-17 16:38:03 +0000

    Criar componente para Pricing

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,7 @@
 import SkillBadge from "../components/badges/SkillBadge";
 import Footer from "../components/layouts/Footer";
 import Header from "../components/layouts/Header";
+import Pricing from "../components/layouts/Pricing";
 import Testimonial from "../components/layouts/Testimonals";
 import CardTutorSmall from "../components/tutor/CardTutorSmall";
 import developers from "../mocks/tutores.json";
@@ -11,7 +12,7 @@ function Home() {
     <>
       <Header />
       <Hero />
-      <Testimonial testimonials={testimonials}/>
+      <Testimonial testimonials={testimonials} />
       <section className="px-8 md:px-0">
         <div className="container mx-auto py-14">
           <h2 className="text-3xl font-bold text-slate-900">
@@ -106,30 +107,6 @@ function Hero() {
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  );
-}
-
-function Pricing() {
-  return (
-    <section className="bg-slate-900 text-white py-14">
-      <div className="container mx-auto flex flex-col justify-center items-center gap-10">
-        <h4 className="md:text-3xl font-bold">
-          Que tal monetizar sua experiência?
-        </h4>
-        <h3 className="text-2xl md:text-4xl font-bold">
-          Torne-se um mentorr especializado
-        </h3>
-        <div className="flex flex-col md:flex-row gap-6">
-          <button className="bg-blue-600 px-12 font-bold py-4 rounded-lg hover:bg-blue-700">
-            Quero ser Mentorr
-          </button>
-          <button className="font-bold border border-blue-600 px-12 py-4 rounded-lg hover:bg-blue-600">
-            Quero ser aluno
-          </button>
-        </div>
-        <p className="text-slate-300">Não é necessário cartão de crédito.</p>
       </div>
     </section>
   );
